e2a92377a4f30aa44c0b68f9db948c044a8d3ceb
Aman <abul2002aman@gmail.com>
2026-03-13 14:40:31 +0530

    2nd commit from sts

--- EmployeeManagementUsingDataJpa/src/main/java/com/example/demo/EmployeeManagementApplication.java
+++ EmployeeManagementUsingDataJpa/src/main/java/com/example/demo/EmployeeManagementApplication.java
@@ -8,6 +8,7 @@ public class EmployeeManagementApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello this is Aman, first line in sts after git");
+		System.out.println("Hello this is Aman, first line in sts after git");
 		SpringApplication.run(EmployeeManagementApplication.class, args);
 	}
 
